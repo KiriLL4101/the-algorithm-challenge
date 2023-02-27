d3860c223a83f7d3af2b7b70c677cbd99835811c
KiriLL4101 <fedechkin-kiril@mail.ru>
2023-02-27 11:44:29 +0300

    LinkedList method delete

--- src/data-structures/LinkedList/LinkedList.spec.ts
+++ src/data-structures/LinkedList/LinkedList.spec.ts
@@ -19,79 +19,79 @@ describe('LinkedList', () => {
     expect(linkedList.tail?.next).toBeNull()
   })
 
-  //   it('should prepend node to linked list', () => {
-  //     const linkedList = new LinkedList()
+  it('should prepend node to linked list', () => {
+    const linkedList = new LinkedList()
 
-  //     linkedList.prepend(2)
-  //     expect(linkedList.head.toString()).toBe('2')
-  //     expect(linkedList.tail.toString()).toBe('2')
+    linkedList.prepend(2)
+    expect(linkedList.head?.toString()).toBe('2')
+    expect(linkedList.tail?.toString()).toBe('2')
 
-  //     linkedList.append(1)
-  //     linkedList.prepend(3)
+    linkedList.append(1)
+    linkedList.prepend(3)
 
-  //     expect(linkedList.toString()).toBe('3,2,1')
-  //   })
+    expect(linkedList.toString()).toBe('3,2,1')
+  })
 
-  //   it('should insert node to linked list', () => {
-  //     const linkedList = new LinkedList()
+  // it('should insert node to linked list', () => {
+  //   const linkedList = new LinkedList()
 
-  //     linkedList.insert(4, 3)
-  //     expect(linkedList.head.toString()).toBe('4')
-  //     expect(linkedList.tail.toString()).toBe('4')
+  //   linkedList.insert(4, 3)
+  //   expect(linkedList.head?.toString()).toBe('4')
+  //   expect(linkedList.tail?.toString()).toBe('4')
 
-  //     linkedList.insert(3, 2)
-  //     linkedList.insert(2, 1)
-  //     linkedList.insert(1, -7)
-  //     linkedList.insert(10, 9)
+  //   linkedList.insert(3, 2)
+  //   linkedList.insert(2, 1)
+  //   linkedList.insert(1, -7)
+  //   linkedList.insert(10, 9)
 
-  //     expect(linkedList.toString()).toBe('1,4,2,3,10')
-  //   })
+  //   expect(linkedList.toString()).toBe('1,4,2,3,10')
+  // })
 
-  //   it('should delete node by value from linked list', () => {
-  //     const linkedList = new LinkedList()
+  it('should delete node by value from linked list', () => {
+    const linkedList = new LinkedList()
 
-  //     expect(linkedList.delete(5)).toBeNull()
+    expect(linkedList.delete(5)).toBeNull()
 
-  //     linkedList.append(1)
-  //     linkedList.append(1)
-  //     linkedList.append(2)
-  //     linkedList.append(3)
-  //     linkedList.append(3)
-  //     linkedList.append(3)
-  //     linkedList.append(4)
-  //     linkedList.append(5)
+    linkedList.append(1)
+    linkedList.append(1)
+    linkedList.append(2)
+    linkedList.append(3)
+    linkedList.append(3)
+    linkedList.append(3)
+    linkedList.append(4)
+    linkedList.append(5)
 
-  //     expect(linkedList.head.toString()).toBe('1')
-  //     expect(linkedList.tail.toString()).toBe('5')
+    expect(linkedList.head?.toString()).toBe('1')
+    expect(linkedList.tail?.toString()).toBe('5')
 
-  //     const deletedNode = linkedList.delete(3)
-  //     expect(deletedNode.value).toBe(3)
-  //     expect(linkedList.toString()).toBe('1,1,2,4,5')
+    const deletedNode = linkedList.delete(3)
+    expect(deletedNode?.value).toBe(3)
+    expect(linkedList.toString()).toBe('1,1,2,4,5')
 
-  //     linkedList.delete(3)
-  //     expect(linkedList.toString()).toBe('1,1,2,4,5')
+    linkedList.delete(3)
+    expect(linkedList.toString()).toBe('1,1,2,4,5')
 
-  //     linkedList.delete(1)
-  //     expect(linkedList.toString()).toBe('2,4,5')
+    linkedList.delete(1)
+    expect(linkedList.toString()).toBe('2,4,5')
 
-  //     expect(linkedList.head.toString()).toBe('2')
-  //     expect(linkedList.tail.toString()).toBe('5')
+    expect(linkedList.head?.toString()).toBe('2')
+    expect(linkedList.tail?.toString()).toBe('5')
 
-  //     linkedList.delete(5)
-  //     expect(linkedList.toString()).toBe('2,4')
+    linkedList.delete(5)
+    expect(linkedList.toString()).toBe('2,4')
 
-  //     expect(linkedList.head.toString()).toBe('2')
-  //     expect(linkedList.tail.toString()).toBe('4')
+    expect(linkedList.head?.toString()).toBe('2')
+    expect(linkedList.tail?.toString()).toBe('4')
 
-  //     linkedList.delete(4)
-  //     expect(linkedList.toString()).toBe('2')
+    linkedList.delete(4)
+    expect(linkedList.toString()).toBe('2')
 
-  //     expect(linkedList.head.toString()).toBe('2')
-  //     expect(linkedList.tail.toString()).toBe('2')
+    expect(linkedList.head?.toString()).toBe('2')
+    expect(linkedList.tail?.toString()).toBe('2')
 
-  //     linkedList.delete(2)
-  //     expect(linkedList.toString()).toBe('')
-  //   })
+    linkedList.delete(2)
+    expect(linkedList.toString()).toBe('')
+  })
 
   //   it('should delete linked list tail', () => {
   //     const linkedList = new LinkedList()
--- src/data-structures/LinkedList/LinkedList.ts
+++ src/data-structures/LinkedList/LinkedList.ts
@@ -10,6 +10,17 @@ export class LinkedList {
     this.compare = new Comparator<number>(comparatorFunction)
   }
 
+  prepend (value: number): this {
+    const newNode = new LinkedListNode(value, this.head ?? undefined)
+    this.head = newNode
+
+    if (!this.tail) {
+      this.tail = newNode
+    }
+
+    return this
+  }
+
   append (value: number): this {
     const newNode = new LinkedListNode(value)
 
@@ -24,6 +35,56 @@ export class LinkedList {
     this.tail = newNode
 
     return this
+  }
+
+  // insert(value: number, pos: number): this {
+  //   const newNode = new LinkedListNode(value)
+
+  //   if (!this.head) {
+  //     this.head = newNode
+  //     this.tail = newNode
+
+  //     return this
+  //   }
+
+  //   let cur = this.head
+
+  //   // while(cur){
+
+  //   //   cur = cur.next
+  //   // }
+
+  //   return this
+  // }
+
+  delete (value: number): LinkedListNode | null {
+    if (!this.head) {
+      return null
+    }
+
+    let deletedNode = null
+
+    while (this.head?.value && this.compare.equal(this.head.value, value)) {
+      deletedNode = this.head
+      this.head = this.head.next
+    }
+
+    let currentNode = this.head
+
+    while (currentNode?.next) {
+      if (currentNode.next.value && this.compare.equal(currentNode.next.value, value)) {
+        deletedNode = currentNode.next
+        currentNode.next = currentNode.next.next
+      } else {
+        currentNode = currentNode.next
+      }
+    }
+
+    if (this.tail?.value && this.compare.equal(this.tail.value, value)) {
+      this.tail = currentNode
+    }
+
+    return deletedNode
   }
 
   toArray (): LinkedListNode[] {
